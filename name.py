583be2a4000ea5464cb4493b86209865d7bb91c1
Gang Liang <lianggangscu@gmail.com>
2015-08-30 15:15:16 -0700

    add __name__ attribut module

---
+++ name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#Filename:name.py
+
+""" test the __name__ attribute
+
+"""
+
+if __name__ =='main':
+	print("This program is being run by itself")
+else:
+	print("I am being in ported from another module")
